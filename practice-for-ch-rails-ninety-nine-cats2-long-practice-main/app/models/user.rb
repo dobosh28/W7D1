29498c1df948b535573367158d71bc88391f09cb
Yosyp Dobosh <joseph.dobosh@gmail.com>
2022-08-29 22:02:19 -0400

    finish part 1

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ApplicationRecord
     validates :username, :session_token, presence: true, uniqueness: true
     validates :password_digest, presence: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
 
     before_validation :ensure_session_token
     
     attr_reader :password
+    
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user.is_password?(password) 
+            return user
+        else
+            nil
+        end
+    end
 
     def password=(password)
         self.password_digest = BCrypt::Password.create(password)
@@ -16,14 +26,6 @@ class User < ApplicationRecord
         password_object.is_password?(password)
     end
 
-    def self.find_by_credentials(username, password)
-        user = User.find_by(username: username)
-        if user.is_password?(password) && user
-            return user
-        else
-            nil
-        end
-    end
 
     def reset_session_token!
         self.session_token = generate_unique_session_token
